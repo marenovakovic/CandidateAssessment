a9ef15dea407cdbb2a91be82c21828d25a6cc03c
markonovakovic <graornovakovic@gmail.com>
2024-02-07 13:43:37 +0100

    Initial commit

---
+++ app/src/main/java/xyz/argent/candidateassessment/app/Constants.kt
@@ -0,0 +1,7 @@
+package xyz.argent.candidateassessment.app
+
+object Constants {
+    const val walletAddress= "0xd8dA6BF26964aF9D7eEd9e03E53415D37aA96045"
+    const val ethExplorerApiKey = "freekey"
+    const val etherscanApiKey = "E5QFXD7ZYRH7THQM5PIXB8JD4GY38SEJZ4"
+}

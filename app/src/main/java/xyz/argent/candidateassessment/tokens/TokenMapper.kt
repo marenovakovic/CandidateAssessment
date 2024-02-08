7c1e37ee5fdd8565b7bd13d54207b50d6900008e
markonovakovic <graornovakovic@gmail.com>
2024-02-08 14:38:59 +0100

    token mapper test. need to test image url mainly

--- app/src/main/java/xyz/argent/candidateassessment/tokens/TokenMapper.kt
+++ app/src/main/java/xyz/argent/candidateassessment/tokens/TokenMapper.kt
@@ -1,4 +1,12 @@
 package xyz.argent.candidateassessment.tokens
 
+const val TokenImageBaseUrl = "https://www.ethplorer.io"
+
 fun EthExplorerApi.TokenResponse.toToken() =
-    Token(address, name, symbol, decimals, image)
+    Token(
+        address = address,
+        name = name,
+        symbol = symbol,
+        decimals = decimals,
+        image = image?.let { "$TokenImageBaseUrl/$it" }.orEmpty(),
+    )
---
+++ app/src/test/kotlin/TokenMapperTest.kt
@@ -0,0 +1,30 @@
+import xyz.argent.candidateassessment.tokens.TokenImageBaseUrl
+import xyz.argent.candidateassessment.tokens.toToken
+import xyz.argent.candidateassessment.tokens.tokensResponse
+import kotlin.test.Test
+import kotlin.test.assertEquals
+
+class TokenMapperTest {
+
+    @Test
+    fun `TokenResponse to Token`() {
+        val response = tokensResponse.tokens.first()
+
+        val token = response.toToken()
+
+        assertEquals(response.address, token.address)
+        assertEquals(response.name, token.name)
+        assertEquals(response.symbol, token.symbol)
+        assertEquals(response.decimals, token.decimals)
+        assertEquals("$TokenImageBaseUrl/${response.image}", token.image)
+    }
+
+    @Test
+    fun `token image link is empty if response image is null`() {
+        val response = tokensResponse.tokens.first().copy(image = null)
+
+        val token = response.toToken()
+
+        assertEquals("", token.image)
+    }
+}

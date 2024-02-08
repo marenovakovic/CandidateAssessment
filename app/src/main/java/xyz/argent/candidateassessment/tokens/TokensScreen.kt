bc4d1dfd24e5341b913edd47e432bc3a823cf9d8
markonovakovic <graornovakovic@gmail.com>
2024-02-08 14:56:07 +0100

    UI improvements

--- app/src/main/java/xyz/argent/candidateassessment/tokens/TokensScreen.kt
+++ app/src/main/java/xyz/argent/candidateassessment/tokens/TokensScreen.kt
@@ -1,18 +1,23 @@
 package xyz.argent.candidateassessment.tokens
 
 import androidx.compose.animation.Crossfade
+import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.PaddingValues
+import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.size
 import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.foundation.lazy.items
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.automirrored.filled.ArrowBack
-import androidx.compose.material3.Card
+import androidx.compose.material3.CardDefaults
 import androidx.compose.material3.CenterAlignedTopAppBar
 import androidx.compose.material3.CircularProgressIndicator
 import androidx.compose.material3.ExperimentalMaterial3Api
@@ -25,12 +30,17 @@ import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.collectAsState
 import androidx.compose.runtime.getValue
+import androidx.compose.runtime.remember
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.clip
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.layout.ContentScale
 import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import androidx.hilt.navigation.compose.hiltViewModel
+import coil.compose.AsyncImage
 import xyz.argent.candidateassessment.R
 import xyz.argent.candidateassessment.balance.Balance
 import xyz.argent.candidateassessment.balance.BalanceState
@@ -73,7 +83,8 @@ private fun TokensScreen(
             horizontalAlignment = Alignment.CenterHorizontally,
             modifier = Modifier
                 .fillMaxSize()
-                .padding(it),
+                .padding(it)
+                .padding(8.dp),
         ) {
             OutlinedTextField(
                 value = state.query,
@@ -124,17 +135,43 @@ private fun Loading() {
 private fun Balances(balances: List<Balance>) {
     LazyColumn(
         horizontalAlignment = Alignment.CenterHorizontally,
+        verticalArrangement = Arrangement.spacedBy(8.dp),
         modifier = Modifier.fillMaxSize(),
     ) {
-        items(balances) {
-            Card(
+        items(balances) { balance ->
+            val color = remember(balance.balance) {
+                balance.balance.fold(
+                    onSuccess = {
+                        when {
+                            it > 0 -> Color(0xFFB6D5D6)
+                            it < 0 -> Color(0xFFE0474C)
+                            else -> Color(0xFFEFEBE9)
+                        }
+                    },
+                    onFailure = { Color(0xFFB11A21) },
+                )
+            }
+            Row(
+                horizontalArrangement = Arrangement.SpaceBetween,
+                verticalAlignment = Alignment.CenterVertically,
                 modifier = Modifier
                     .fillMaxWidth()
-                    .padding(4.dp),
+                    .clip(CardDefaults.shape)
+                    .background(color)
+                    .padding(16.dp),
             ) {
-                Text(
-                    text = it.token.name.orEmpty(),
-                    modifier = Modifier.padding(16.dp),
+                Column(verticalArrangement = Arrangement.Center) {
+                    AsyncImage(
+                        model = balance.token.image,
+                        contentDescription = null,
+                        contentScale = ContentScale.FillBounds,
+                        modifier = Modifier.size(32.dp)
+                    )
+                    Text(text = balance.token.name.orEmpty())
+                }
+                balance.balance.fold(
+                    onSuccess = { Text(text = it.toString()) },
+                    onFailure = { Text(text = "Error occurred") },
                 )
             }
         }

f0ebc56cf3bf2491a1d346dd51d8102b4bd30edb
markonovakovic <graornovakovic@gmail.com>
2024-02-13 13:00:44 +0100

    formatting token balance

--- app/src/main/java/xyz/argent/candidateassessment/balance/Balance.kt
+++ app/src/main/java/xyz/argent/candidateassessment/balance/Balance.kt
@@ -1,8 +1,24 @@
 package xyz.argent.candidateassessment.balance
 
+import java.math.RoundingMode
 import xyz.argent.candidateassessment.tokens.Token
 
-data class Balance(
+data class Balance private constructor(
     val token: Token,
     val balance: Result<Double>,
-)
+) {
+
+    companion object {
+        operator fun invoke(token: Token, balance: Result<Double>) =
+            Balance(
+                token,
+                balance
+                    .map {
+                        it
+                            .toBigDecimal()
+                            .setScale(token.decimals?.toInt() ?: 0, RoundingMode.HALF_EVEN)
+                            .toDouble()
+                    },
+            )
+    }
+}

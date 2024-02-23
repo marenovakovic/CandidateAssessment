2ce84315e49b9435c357964cfc19c914fa081920
markonovakovic <graornovakovic@gmail.com>
2024-02-23 10:34:23 +0100

    revert to old solution for sake of time

--- app/src/main/java/xyz/argent/candidateassessment/tokens/TokensViewModel.kt
+++ app/src/main/java/xyz/argent/candidateassessment/tokens/TokensViewModel.kt
@@ -1,5 +1,5 @@
 @file:Suppress("FunctionName")
-@file:OptIn(ExperimentalCoroutinesApi::class, FlowPreview::class)
+@file:OptIn(ExperimentalCoroutinesApi::class)
 
 package xyz.argent.candidateassessment.tokens
 
@@ -7,32 +7,40 @@ import androidx.lifecycle.SavedStateHandle
 import androidx.lifecycle.ViewModel
 import dagger.hilt.android.lifecycle.HiltViewModel
 import javax.inject.Inject
+import kotlinx.collections.immutable.ImmutableList
 import kotlinx.collections.immutable.toImmutableList
 import kotlinx.coroutines.ExperimentalCoroutinesApi
-import kotlinx.coroutines.FlowPreview
+import kotlinx.coroutines.flow.MutableStateFlow
 import kotlinx.coroutines.flow.SharingStarted
 import kotlinx.coroutines.flow.combine
-import kotlinx.coroutines.flow.debounce
-import kotlinx.coroutines.flow.filterNotNull
-import kotlinx.coroutines.flow.flatMapLatest
+import kotlinx.coroutines.flow.distinctUntilChanged
+import kotlinx.coroutines.flow.filter
+import kotlinx.coroutines.flow.filterIsInstance
+import kotlinx.coroutines.flow.flowOf
+import kotlinx.coroutines.flow.map
 import kotlinx.coroutines.flow.mapLatest
 import kotlinx.coroutines.flow.onEach
+import kotlinx.coroutines.flow.onStart
 import kotlinx.coroutines.flow.stateIn
+import kotlinx.coroutines.flow.update
+import kotlinx.coroutines.launch
 import xyz.argent.candidateassessment.CloseableCoroutineScope
+import xyz.argent.candidateassessment.balance.Balance
 import xyz.argent.candidateassessment.balance.BalancesState
 import xyz.argent.candidateassessment.balance.GetBalances
-import xyz.argent.candidateassessment.balance.ObserveBalances
-import xyz.argent.candidateassessment.balance.RefreshBalances
 import xyz.argent.candidateassessment.connectivity.ConnectivityObserver
+import xyz.argent.candidateassessment.connectivity.flatMapLatest
 
 sealed interface TokensState {
     data object Initial : TokensState
     data object Loading : TokensState
     data class Tokens(
         val query: String,
+        val tokens: List<Token>,
         val balancesState: BalancesState,
     ) : TokensState
 
+    data object ConnectivityError : TokensState
     data object Error : TokensState
 }
 
@@ -43,38 +51,85 @@ class TokensViewModel @Inject constructor(
     connectivityObserver: ConnectivityObserver,
     private val getTokens: GetTokens,
     getBalances: GetBalances,
-    observeTokens: ObserveTokens,
-    observeBalances: ObserveBalances,
 ) : ViewModel(coroutineScope) {
 
-    private val query = savedStateHandle.getStateFlow<String?>(QUERY, null)
+    private val query = savedStateHandle.getStateFlow(QUERY, "")
+    private val tokens = MutableStateFlow<TokensState>(TokensState.Initial)
 
-    private val balances =
-        observeTokens()
-            .flatMapLatest { tokens ->
-                query
-                    .filterNotNull()
-                    .debounce(500)
-                    .mapLatest { query -> tokens.search(query) }
+    private val tokensState =
+        combine(query, tokens) { query, tokensState ->
+            when (tokensState) {
+                is TokensState.Tokens ->
+                    TokensState.Tokens(
+                        query = query,
+                        tokens = tokensState.tokens.search(query),
+                        balancesState = BalancesState.Initial,
+                    )
+                else -> tokensState
             }
-            .onEach(observeBalances::refresh)
-            .flatMapLatest(observeBalances)
+        }
 
-    val state = combine(query, balances) { query, balances ->
-        TokensState.Tokens(query.orEmpty(), BalancesState.Success(balances.toImmutableList()))
-    }
-        .stateIn(coroutineScope, SharingStarted.WhileSubscribed(5_000), TokensState.Initial)
+    private val loadingBalances = MutableStateFlow(false)
+
+    private val searchedTokens =
+        tokensState
+            .filterIsInstance<TokensState.Tokens>()
+            .filter { it.query.isNotBlank() }
+            .mapLatest { it.tokens }
+            .distinctUntilChanged()
+
+    private val balancesState =
+        searchedTokens
+            .onEach { loadingBalances.update { true } }
+            .mapLatest(getBalances)
+            .map(List<Balance>::toImmutableList)
+            .map(BalancesState::Success)
+            .onEach { loadingBalances.update { false } }
+            .onStart<BalancesState> { emit(BalancesState.Initial) }
+
+    private val _state =
+        combine(
+            tokensState,
+            balancesState,
+            loadingBalances,
+        ) { tokensState, balances, loadingBalances ->
+            when (tokensState) {
+                is TokensState.Tokens -> {
+                    tokensState.copy(
+                        balancesState =
+                        when {
+                            loadingBalances -> BalancesState.Loading
+                            balances is BalancesState.Success -> balances
+                            else -> tokensState.balancesState
+                        },
+                    )
+                }
+                else -> tokensState
+            }
+        }
+
+    val state =
+        connectivityObserver
+            .status
+            .flatMapLatest(
+                onUnavailable = { flowOf(TokensState.ConnectivityError) },
+                onAvailable = { _state },
+            )
+            .stateIn(coroutineScope, SharingStarted.WhileSubscribed(5_000), TokensState.Initial)
 
     fun init() {
-
+        tokens.update { TokensState.Loading }
+        coroutineScope.launch {
+            tokens.update { loadTokens() }
+        }
     }
 
-//    private suspend fun loadTokens() =
-//        getTokens()
-//            .fold(
-//                onFailure = { TokensState.Error },
-//                onSuccess = { TokensState.Tokens("", BalancesState.Initial) },
-//            )
+    private suspend fun loadTokens() =
+        getTokens()
+            .fold(
+                onFailure = { TokensState.Error },
+                onSuccess = { TokensState.Tokens("", it, BalancesState.Initial) },
+            )
 
     fun retry() = init()
 
